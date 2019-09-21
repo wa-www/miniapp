18d915fb83a91305fb7471de3bc149f3e8018783
Wa-www <0nicoco0@gmail.com>
2019-09-22 00:42:00 +0900

    機能の実装
    機能の実装（あとはview画面編集）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'blog#index'
+  resources :blog , only: [:index, :new, :create, :update, :destroy, :edit]
 end
