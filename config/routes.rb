b42a8b3cb54d28f6b10ec50e5ddc9887cd460be2
Wa-www <0nicoco0@gmail.com>
2019-09-26 23:49:38 +0900

    material_dezign
    マテリアライズ修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'blog#index'
   resources :blog , only: [:index, :new, :create, :update, :destroy, :edit]
-  get '/users/sign_out' => 'sessions#destroy'
+  # ログアウト
+  # get 'users/sign_out' => 'devise/sessions#destroy'
+  # get '/users/sign_out' => 'sessions#destroy'
+  # 日記削除
+  # get '/blog/:id' => 'blog#destroy'
 end
