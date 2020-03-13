2ea3356eb8eed3385dd0e527b7ad004e547731b3
Wa-www <0nicoco0@gmail.com>
2020-03-13 13:29:45 +0900

    i
    o

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// = require rails-ujs
-// = require jquery
-// = require jquery_ujs
-// = require bootstrap.min
-// = require turbolinks
-// = require_tree .
-// = require materialize
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
 
